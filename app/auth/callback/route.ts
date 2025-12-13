0657635735ed56866ac6b154a52d8f731e6f7325
Oladehinde Ibrahim <brahimoladehinde1@gmail.com>
2025-12-13 15:10:48 +0100

    Tried to update the supabase model to a lesser version

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,6 +1,6 @@
 // app/auth/callback/route.ts
 
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
@@ -12,7 +12,14 @@ export async function GET(request: NextRequest) {
   const code = requestUrl.searchParams.get("code");
 
   if (code) {
-    const supabase = createRouteHandlerClient({ cookies });
+    const supabase = createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL || "", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "", {
+      cookies: {
+        getAll: async () => {
+          const all = request.cookies.getAll();
+          return all.map((c: any) => ({ name: c.name, value: c.value }));
+        },
+      },
+    });
     await supabase.auth.exchangeCodeForSession(code);
   }
 
